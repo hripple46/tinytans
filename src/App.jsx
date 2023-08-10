2c515373627d5f741ef171230df737c3744a0577
Henry Ripple <hripple46@gmail.com>
2023-08-10 14:45:08 -0500

    moved assets to public

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,22 @@ function App() {
       >
         <img
           className="w-32 h-48"
-          src="./src/assets/Jh_Jin_Jk_Jm/JH/JH_COLORS.png"
+          src="/dist/assets/Jh_Jin_Jk_Jm/JH/JH_COLORS.png"
         ></img>
         <img
           className="w-32 h-48"
-          src="./src/assets/Jh_Jin_Jk_Jm/JIN/JIN_COLORS.png"
+          src="../dist/assets/Jh_Jin_Jk_Jm/JIN/JIN_COLORS.png"
         ></img>
         <img
           className="w-32 h-48"
-          src="./src/assets/Jh_Jin_Jk_Jm/JK/JK_COLORS.png"
+          src="../dist/assets/Jh_Jin_Jk_Jm/JK/JK_COLORS.png"
         ></img>
         <div className="relative">
           <img
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             className="relative w-32 h-48"
-            src="./src/assets/Jh_Jin_Jk_Jm/JM/JM_COLORS.png"
+            src="../dist/assets/Jh_Jin_Jk_Jm/JM/JM_COLORS.png"
           ></img>
           <h1 className="absolute -top-full right-2 text-white text-2xl">
             We Love You, Ashmita!
@@ -38,15 +38,15 @@ function App() {
         </div>
         <img
           className="w-32 h-48"
-          src="./src/assets/Rm_Sg_V/RM/RM_COLORS.png"
+          src="/dist/assets/Rm_Sg_V/RM/RM_COLORS.png"
         ></img>
         <img
           className="w-32 h-48"
-          src="./src/assets/Rm_Sg_V/SUGA/SUGA_COLORS.png"
+          src="../dist/assets/Rm_Sg_V/SUGA/SUGA_COLORS.png"
         ></img>
         <img
           className="w-32 h-48"
-          src="./src/assets/Rm_Sg_V/TH/TH_COLORS.png"
+          src="../dist/assets/Rm_Sg_V/TH/TH_COLORS.png"
         ></img>
       </div>
     </>
